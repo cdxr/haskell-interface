053881e7221cea9e8dd7f08abe14203bcfec55e6
Craig Roche <cdxr01@gmail.com>
2015-06-13 17:50:01 -0700

    fixed haddock error

--- src/Data/Interface/Source.hs
+++ src/Data/Interface/Source.hs
@@ -12,11 +12,11 @@ data Source = Source !FilePath !SrcSpan
     deriving (Show, Read, Eq, Ord)
 
 data SrcSpan = SrcSpan
-    {-# UNPACK #-} !SrcLoc  -- ^ start source location
-    {-# UNPACK #-} !SrcLoc  -- ^ one past end source location
+    {-# UNPACK #-} !SrcLoc  -- start source location
+    {-# UNPACK #-} !SrcLoc  -- one past end source location
     deriving (Show, Read, Eq, Ord)
 
 data SrcLoc = SrcLoc
-    {-# UNPACK #-} !Int  -- ^ source line
-    {-# UNPACK #-} !Int  -- ^ source column
+    {-# UNPACK #-} !Int  -- source line
+    {-# UNPACK #-} !Int  -- source column
     deriving (Show, Read, Eq, Ord)
