c170b8191620ccb1b1d8274904c107b90034529d
Craig Roche <cdxr01@gmail.com>
2015-08-19 10:41:10 -0700

    implement temporary workaround for sandbox package issue

--- src/LoadPackageInterface.hs
+++ src/LoadPackageInterface.hs
@@ -67,37 +67,58 @@ makePackageInterface debugInfo lp@(LocPackage _ ipi) = do
 
         let pkgKey = packageKey ipi
         exposed <- makeModuleEnv pkgKey $ map exposedName (exposedModules ipi)
-        --hidden  <- makeModuleEnv pkgKey $ hiddenModules ipi
-        let hidden = mempty
+        let hidden = mempty -- TODO
         pure $ PackageInterface
             { pkgId             = sourcePackageId ipi
             , pkgExposedModules = exposed
             , pkgHiddenModules  = hidden
             }
 
--- | Expose a package to GHC
+-- | Expose a single package to GHC. Invoking `ghcLocPackage` again overwrites
+-- the effects of previous calls.
 ghcLocPackage :: LocPackage -> Ghc ()
 ghcLocPackage (LocPackage db ipi) = do
+    {- Workaround  (TODO)
+
+        This is a temporary hack to work around some confusing GHC behavior.
+        When the given package database is either the global or the user
+        package database, the equivalent of a "-package-key" flag is used to
+        ensure that the package is visible. This is done with the
+        `exposePackage` function defined below. If `exposePackage` was not
+        used for the global and user databases, then a package would only be
+        visible if there was not a more recent version of that package in the
+        database.
+
+        `exposePackage` *should* also be applied when given a path to a
+        specific package database (for example when the package is in a
+        sandbox).  However, doing so consistently results in the error
+        "cannot satisfy -package-key PACKAGE_KEY". As a temporary workaround,
+        the "-package-key" flag is not provided in this case. However, this
+        means that when multiple versions of a package are installed in a
+        sandbox only one version will work, and the others will result in the
+        above error.
+    -}
     dflags0 <- getSessionDynFlags
-    pkgKeys <- setSessionDynFlags dflags0
-            { extraPkgConfs = (toPkgConfRef db :)
-            , packageFlags = [ ExposePackage (pkgArg ipi) modRenaming ]
-            }
-    --liftIO $ GHC.linkPackages dflags0 pkgKeys
+    let dflags = case db of
+            GlobalPackageDB -> exposePackage dflags0
+            UserPackageDB -> exposePackage dflags0
+            SpecificPackageDB path -> addPackagePath path dflags0
+
+    _pkgKeys <- setSessionDynFlags dflags
 
     return ()
   where
-    toPkgConfRef :: PackageDB -> PkgConfRef
-    toPkgConfRef db = case db of
-        GlobalPackageDB      -> GlobalPkgConf
-        UserPackageDB        -> UserPkgConf
-        SpecificPackageDB fp -> PkgConfFile fp
+    exposePackage dflags =
+        let pflag = ExposePackage (pkgArg ipi) noRenaming
+        in dflags { packageFlags = pflag : packageFlags dflags }
+
+    addPackagePath path dflags =
+        dflags { extraPkgConfs = (PkgConfFile path :) }
 
     pkgArg :: InstalledPackageInfo -> PackageArg
     pkgArg = PackageKeyArg . Cabal.display . packageKey
-    --pkgArg = PackageIdArg . Cabal.display . Cabal.installedPackageId
 
-    modRenaming = ModRenaming True []
+    noRenaming = ModRenaming True []
 
 
 debugPackageFlags :: Ghc ()
